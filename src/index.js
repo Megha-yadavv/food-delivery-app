9d4cf32db6a1cf1d01aa9769d0406329337b2394
Megha-yadavv <87147954+Megha-yadavv@users.noreply.github.com>
2024-10-28 00:36:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import StoreContextProvider from './Context/StoreContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StoreContextProvider>
-     <BrowserRouter>
+     <HashRouter>
    <App />
-  </BrowserRouter>
+  </HashRouter>
   </StoreContextProvider>
  
 );
